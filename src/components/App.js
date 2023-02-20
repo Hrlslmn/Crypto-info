3adbd8ec6e1e65cb8808f2c4de1f6435e0e82b7e
Hairulslmn <sulaimanhairul@gmail.com>
2023-02-20 22:59:41 +0800

    i

--- src/components/App.js
+++ src/components/App.js
@@ -38,7 +38,7 @@ function App() {
 
   return (
     <>
-      <h1>hello</h1>
+      <h1>helloo</h1>
       <Header/>
       <div className='main-container'>
         <h1 className='crypto-title '>Enquire Crypto Price</h1>
